7c32793c83010709a481fabdf4caa401e782368d
Alexey1307 <bachurin1307@gmail.com>
2022-03-22 14:54:15 +0300

    commit

--- calendar.js
+++ calendar.js
@@ -7,42 +7,17 @@ document.querySelector('.button').onclick = function(){
 
     let out = document.querySelector('.out');
     let out_2 = document.querySelector('.out_2');
+    let out_3 = document.querySelector('.out_3');
 
     let count = 0;
     for(let i = dateStart; i <= dateEnd; i = i + 24*60*60*1000){
-        out.innerHTML += new Date(i).toISOString().substring(0, 10) + 
-        '<br>';
+        let arg = [new Date(i).toISOString().substring(0, 10)];
+        out.innerHTML += arg;
+        console.log(arg[0]); //Почему ты лять не выводишь 1 элемент???
+        // out.innerHTML = new Date(i).toISOString().substring(0, 10) + '<br>';
         count ++;
-        out_2.innerHTML = count;
-        console.log(count);
+        let trueCount = count -1;
+        out_2.innerHTML = trueCount;
+        out_3.innerHTML = trueCount * 2000 + "р";
         }
     };
-
-// let count = 0;
-
-// for (let i = 0; i < 27; i++) {
-//     // console.log(i);
-//     count++;
-// }
-
-// console.log(count);
-
-
-
-
-// function calc () {
-//     let dateStart = document.querySelector('.start_date').value;
-//     let dateEnd = document.querySelector('.end_date').value;
-//     // dateStart = Date.parse(dateStart);
-//     // dateEnd =Date.parse(dateEnd);
-//     const oneDay = 24 * 60 * 60 * 1000;
-//     // Calculating the time difference between two dates
-//     let a = dateStart.getTime();
-//     let b = dateEnd.getTime()
-//     const diffInTime = a - b;
-//     // Calculating the numbers of days between two dates
-//     const diffInDays = Math.round(diffInTime / oneDay);
-//     let out = document.querySelector('.out');
-//     return diffInDays;
-
-// }
