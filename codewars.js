d9ec1e60064523bf2fbf138a07dc7be662df5a39
Alexey1307 <bachurin1307@gmail.com>
2022-03-26 21:22:43 +0300

    comit

--- codewars.js
+++ codewars.js
@@ -1,7 +1,28 @@
-let arr = ['html', 3, 5];
-// console.log(arr);
+let arr = [1,2,3,4,6,6,7,8];
+// function firstNonConsecutive(arr) {
+//   for(let i in arr){
+//     if (i!=i+10) {
+//       console.log(i)
+//     }
+//     else{
+//       return null;
+//     }
+//   }
+// }
 
-//forEach
-let arr_2 = arr.forEach(function (item, i, arr) {
-  console.log(item[0]);
-});
+
+
+
+function firstNonConsecutive(arr){
+  for (let i =  0; i <= arr.length; i++) {
+      let arr_2 = arr[i] +1;
+      if (arr_2[i]-1 == arr) {
+        console.log(arr_2);
+      }
+      
+    }
+  }
+firstNonConsecutive(arr);
+
+
+// If the whole array is consecutive then return null.
