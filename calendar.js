a6fd9d6d66a0826b318e0287d4b282e56dbef7ce
Alexey1307 <bachurin1307@gmail.com>
2022-03-28 10:44:16 +0300

    js

--- calendar.js
+++ calendar.js
@@ -9,6 +9,9 @@ document.querySelector('.button').addEventListener('click', function(){
     let out_2 = document.querySelector('.out_2');
     let out_3 = document.querySelector('.out_3');
     let count = 0;
+    let select = document.querySelector('select').selectedIndex;
+    let selectValue = select + 1; // selectedIndex считается с 0, поэтому +1
+
 
     const startDateRender = new Date(dateStart).toLocaleDateString().substring(0, 10);
     const endDateRender = new Date(dateEnd).toLocaleDateString().substring(0, 10);
@@ -18,6 +21,7 @@ document.querySelector('.button').addEventListener('click', function(){
         count++;
         let trueCount = count - 1;
         out_2.innerHTML = `${trueCount}`; //количесвто дней
-        out_3.innerHTML = trueCount * 2000 + "р"; //сумма
+        out_3.innerHTML = trueCount * 2000 * selectValue  + "р"; //сумма
     }
     });
+
