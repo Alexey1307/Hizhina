ffc050a604e9a045660163954ccf325fc1ff7e94
Alexey1307 <bachurin1307@gmail.com>
2022-03-30 14:06:59 +0300

    commit

--- codewars.js
+++
@@ -1,7 +0,0 @@
-let arr = [1,2,3,4,6,8];
-найди все четные и нечетные числа и потом напиши условие,
-что если четно - нечетное больше 1 - breack и return это число. или без breck
-попробуй и так и так.
-
-
-// If the whole array is consecutive then return null.
