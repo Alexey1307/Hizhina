82e35cfb3c87a58a802de204976d75b749c0e1e0
Alexey1307 <bachurin1307@gmail.com>
2022-04-12 13:27:58 +0300

    js

--- main.js
+++ main.js
@@ -54,17 +54,25 @@ const swiper = new Swiper('.swiper', {
 
 //scrolling
 // calendar
+let btnCalendar = document.querySelector('.buttonCalendar');
 let calendarElement = document.querySelector(".calendar");
-let btnCalendar = document.querySelector('.linksCalendar');
 	function bookingClick() {
 		calendarElement.scrollIntoView({ block: "center", behavior: "smooth" });
 	}
 btnCalendar.addEventListener('click', bookingClick);
 
 // rooms
+let btnRooms = document.querySelector('.buttonRooms');
 let roomsElement = document.querySelector(".roomsDescription");
-let btnRooms = document.querySelector('.linksRooms');
 	function roomsClick() {
 		roomsElement.scrollIntoView({ block: "center", behavior: "smooth" });
 	}
 btnRooms.addEventListener('click', roomsClick);
+
+// contacts
+let btnContact = document.querySelector('.buttonContact');
+let contactElement = document.querySelector(".footer");
+function contactClick() {
+	contactElement.scrollIntoView({ block: "center", behavior: "smooth" });
+}
+btnContact.addEventListener('click', contactClick);
